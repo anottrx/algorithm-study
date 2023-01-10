b45270dba710f0b98797af96177dc50ec49f3cbc
evencoding <evencoding@gmail.com>
2023-01-10 19:28:00 +0900

    solve: 프로그래머스 위장

---
+++ level-2/위장/류정우.js
@@ -0,0 +1,21 @@
+const getClothesObj = (clothes) => {
+  return clothes.reduce((acc, [name, type]) => {
+    if (acc[type]) {
+      acc[type].push(name);
+      return acc;
+    }
+    acc[type] = [name];
+    return acc;
+  }, {});
+};
+
+function solution(clothes) {
+  var answer = 1;
+
+  const clothesObj = getClothesObj(clothes);
+  Object.values(clothesObj).forEach((clothes) => {
+    answer *= clothes.length + 1;
+  });
+
+  return answer - 1;
+}
