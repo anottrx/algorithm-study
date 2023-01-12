e8f271a6c21c2e136c50e31c08d96974e4451c9b
evencoding <evencoding@gmail.com>
2023-01-12 23:43:14 +0900

    refactor(메뉴리뉴얼): 추가할 코스 메뉴를 반환하는 기능을 함수로 분리

--- level-2/메뉴리뉴얼/류정우.js
+++ level-2/메뉴리뉴얼/류정우.js
@@ -25,7 +25,7 @@ const addCourse = (orders, menuCount, courses) => {
   return courses;
 };
 
-const getCourses = (orders, course) => {
+const getCourseCandidates = (orders, course) => {
   let courses = {};
 
   course.forEach((menuCount) => {
@@ -35,22 +35,27 @@ const getCourses = (orders, course) => {
   return courses;
 };
 
-function solution(orders, course) {
-  const answer = [];
-
-  const sortedOrders = orders.map((order) => [...order].sort().join(''));
-  const courses = getCourses(sortedOrders, course);
+const getCourses = (courseCandidates, course) => {
+  const courses = [];
 
   course.forEach((menuCount) => {
-    const targetCourses = Object.entries(courses).filter(
+    const targetCourses = Object.entries(courseCandidates).filter(
       ([key, value]) => key.length === menuCount && value >= 2
     );
     const max = Math.max(...targetCourses.map(([_, value]) => value));
 
     targetCourses.forEach(([key, value]) => {
-      if (value === max) answer.push(key);
+      if (value === max) courses.push(key);
     });
   });
 
-  return answer.sort();
+  return courses;
+};
+
+function solution(orders, course) {
+  const sortedOrders = orders.map((order) => [...order].sort().join(''));
+  const courseCandidates = getCourseCandidates(sortedOrders, course);
+  const courses = getCourses(courseCandidates, course);
+
+  return courses.sort();
 }
