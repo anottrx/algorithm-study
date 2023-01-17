a227b1701019e924f97e9e863bf69f187b78223e
joseph-106 <sjjoseph106@gmail.com>
2023-01-17 19:28:10 +0900

    solve: 프로그래머스 타겟넘버

---
+++ level-2/타겟 넘버/김성중.js
@@ -0,0 +1,14 @@
+function solution(numbers, target) {
+  let count = 0;
+
+  const recursive = (sum, loc) => {
+    if (loc < numbers.length) {
+      recursive(sum + numbers[loc], loc + 1);
+      recursive(sum - numbers[loc], loc + 1);
+    } else if (sum === target) count += 1;
+  };
+
+  recursive(0, 0);
+
+  return count;
+}
