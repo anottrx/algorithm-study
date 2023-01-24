4d0bd489d45e60940f00bf033aa81142c3714d88
ryuna <anottrx@gmail.com>
2023-01-25 01:21:43 +0900

    refactor: 프로그래머스 가장 큰 수

--- level-2/가장큰수/구련아.js
+++ level-2/가장큰수/구련아.js
@@ -1,8 +1,6 @@
 function solution(numbers) {
   const answer = numbers
-    .sort(
-      (a, b) => Number(String(b) + String(a)) - Number(String(a) + String(b))
-    )
+    .sort((a, b) => b + String(a) - (a + String(b)))
     .join("");
   return answer.startsWith("0") ? "0" : answer;
 }
