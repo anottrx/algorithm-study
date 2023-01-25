82bc52e3a73dd711ac3fb3b7d6dbdd8f0fdccf97
ryuna <anottrx@gmail.com>
2023-01-25 21:32:07 +0900

    solve: 프로그래머스 가장 먼 노드

---
+++ level-3/가장먼노드/구련아.js
@@ -0,0 +1,31 @@
+function solution(n, edge) {
+  const nodes = new Map(); // key:시작점, values:[연결된 노드들]
+  edge.forEach(([start, end]) => {
+    // 양방향
+    nodes.set(start, nodes.has(start) ? [...nodes.get(start), end] : [end]);
+    nodes.set(end, nodes.has(end) ? [...nodes.get(end), start] : [start]);
+  });
+
+  const visited = Array.from({ length: n + 1 }, () => false); // 방문 체크
+  const dists = Array.from({ length: n + 1 }, () => 0); // 거리 길이
+
+  const stacks = [1]; // 1부터 시작
+  visited[1] = true;
+  while (stacks.length > 0) {
+    // bfs
+    const start = stacks.shift();
+    const ends = nodes.get(start);
+    ends.forEach((end) => {
+      if (!visited[end]) {
+        visited[end] = true;
+        dists[end] = dists[start] + 1;
+        stacks.push(end);
+      }
+    });
+  }
+
+  return dists.reduce(
+    (prev, cur) => prev + (cur === Math.max(...dists) ? 1 : 0),
+    0
+  );
+}
