4438243bf9767d8fb50070ad19f846fd340f0272
evencoding <evencoding@gmail.com>
2023-01-27 16:33:43 +0900

    fix(프로그래머스 가장 큰 수): '0'이 반복될 때 '0'을 반환하도록 수정

--- level-2/가장큰수/류정우.js
+++ level-2/가장큰수/류정우.js
@@ -1,7 +1,10 @@
 function solution(numbers) {
-  const sortedNumbers = numbers.map(String).sort((a, b) => {
-    return b.repeat(4).substr(0, 4) - a.repeat(4).substr(0, 4);
-  });
+  const sortedNumber = numbers
+    .map(String)
+    .sort((a, b) => {
+      return b.repeat(4).substr(0, 4) - a.repeat(4).substr(0, 4);
+    })
+    .join('');
 
-  return sortedNumbers.join('');
+  return sortedNumber[0] === '0' ? '0' : sortedNumber;
 }
