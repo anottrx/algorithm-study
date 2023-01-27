7a84b1f5a943318d83bc35ca6afb117ba2843ee2
evencoding <evencoding@gmail.com>
2023-01-27 15:57:12 +0900

    refactor(프로그래머스 가장 먼 노드): bfs 로직을 함수로 분리

--- level-3/가장먼노드/류정우.js
+++ level-3/가장먼노드/류정우.js
@@ -1,3 +1,24 @@
+const bfs = (graph, start, n) => {
+  const visited = Array.from({ length: n + 1 }, () => false);
+  const dist = Array.from({ length: n + 1 }, () => 0);
+  const queue = [start];
+
+  while (queue.length > 0) {
+    const curNode = queue.shift();
+    if (visited[curNode]) continue;
+    visited[curNode] = true;
+
+    graph[curNode].forEach((nextNode) => {
+      if (dist[nextNode] === 0 && nextNode !== 1) {
+        dist[nextNode] = dist[curNode] + 1;
+        queue.push(nextNode);
+      }
+    });
+  }
+
+  return dist;
+};
+
 const getGraph = (n, edge) => {
   const graph = Array.from({ length: n + 1 }, () => []);
   edge.forEach(([a, b]) => {
@@ -10,20 +31,8 @@ const getGraph = (n, edge) => {
 
 function solution(n, edge) {
   const graph = getGraph(n, edge);
-  const visited = Array.from({ length: n + 1 }, () => false);
-  const dist = Array.from({ length: n + 1 }, () => 0);
-  const queue = [1];
-  while (queue.length > 0) {
-    const curNode = queue.shift();
-    if (visited[curNode]) continue;
-    visited[curNode] = true;
-    graph[curNode]?.forEach((nextNode) => {
-      if (dist[nextNode] === 0 && nextNode !== 1) {
-        dist[nextNode] = dist[curNode] + 1;
-        queue.push(nextNode);
-      }
-    });
-  }
+  const dist = bfs(graph, 1, n);
   const max = Math.max(...dist);
+
   return dist.filter((d) => d === max).length;
 }
