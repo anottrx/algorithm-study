2149b3057989e79891e599d4951717e1e11d5a89
osdoonhyun <skw1996@naver.com>
2023-02-10 21:43:19 +0900

    progress(2xn타일링): 시간초과

--- PROGRAMMERS/level-2/2xn타일링/신승현.js
+++ PROGRAMMERS/level-2/2xn타일링/신승현.js
@@ -1,25 +1,9 @@
 function solution(n) {
-  // let answer = 0;
-  let answer = 0;
-
-  const getPermutations = function (arr, selectNumber) {
-    const results = [];
-    if (selectNumber === 1) return arr.map((el) => [el]);
-
-    arr.forEach((fixed, index, origin) => {
-      const rest = [...origin.slice(0, index), ...origin.slice(index + 1)];
-      const permutations = getPermutations(rest, selectNumber - 1);
-      const attached = permutations.map((el) => [fixed, ...el]);
-      results.push(...attached);
-    });
-  };
-
-  for (let i = 0; i <= Math.floor(n / 2); i++) {
-    const arr = Array.from({ length: n - i }, () => 1);
-    arr.fill(2, 0, i);
-    answer += getPermutations(arr, arr.length);
+  const mod = 1_000_000_007;
+  let dp = [1, 1, 2];
+  for (let i = 3; i <= n; i++) {
+    dp[i] = (dp[i - 1] + dp[i - 2]) % mod;
   }
-
-  return answer;
+  return dp[n] % mod;
 }
-// 1,2로만 구성된 배열을 구하고 순열로 갯수 구해서 풀려했으나 실패
+// 어떻게 더 줄일 수 있을지 모르겠음
