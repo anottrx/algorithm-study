d7260f7ebb73566e4b2788d33144b2cb8078ab89
joseph-106 <sjjoseph106@gmail.com>
2023-02-14 20:54:23 +0900

    refactor(3xn타일링): 오탈자 수정

--- PROGRAMMERS/level-2/3xn타일링/김성중.js
+++ PROGRAMMERS/level-2/3xn타일링/김성중.js
@@ -2,13 +2,13 @@ function solution(n) {
   if (n % 2 === 1 || n < 2) return 0;
 
   const MOD = 1_000_000_007;
-  const normarlize = ([prev, res]) =>
+  const normalize = ([prev, res]) =>
     (((4 * res) % MOD) - (prev % MOD) + MOD) % MOD;
 
   let [prev, res] = [3, 11];
 
   for (let i = 2; i <= n / 2 - 1; i++)
-    [prev, res] = [res, normarlize([prev, res])];
+    [prev, res] = [res, normalize([prev, res])];
 
   return res;
 }
